66ba274b49b2148e3293f45c0a53093b4ae2d8f8
ARegadas <apregadas@gmail.com>
2017-03-05 20:44:08 +0000

    server and client app working in same port

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
 
-var port = 3000;
+//var port = 3000;
 
 var app = express();
 
 //View Engine
-app.set('views', path.join(__dirname, 'index'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+//app.set('views', path.join(__dirname, 'index'));
+//app.set('view engine', 'ejs');
+//app.engine('html', require('ejs').renderFile);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 //Body parser MW
 app.use(bodyParser.json());
@@ -24,6 +24,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', index);
 app.use('/api', tasks);
 
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = process.env.PORT || '4200';
+app.set('port', port);
 
 app.listen(port, function(){
   console.log('Server started on port ' + port);
